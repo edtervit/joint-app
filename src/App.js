242b413b82562c18f4aeee2e50b39736631ffa0d
Ed Tervit <edtervit@hotmail.co.uk>
2020-12-12 22:09:32 +0000

    added get playlists

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   //state
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [profile, setProfile] = useState({});
+  const [playlists, setPlaylists] = useState({});
+  const [hasPlaylists, setHasPlaylists] = useState(false);
+
   //useeffect
   useEffect(() => {
     let token = queryString.parse(window.location.search);
@@ -21,9 +24,30 @@ function App() {
         .then((data) => {
           setProfile(data.data);
           setIsLoggedIn(true);
+          console.log(data);
         });
     }
   }, []);
+
+  //handlers
+
+  const getPlaylistHandler = async () => {
+    let token = queryString.parse(window.location.search);
+    let parsedToken = token.access_token;
+    if (parsedToken) {
+      axios
+        .get("https://api.spotify.com/v1/me/playlists?limit=50", {
+          headers: {
+            Authorization: `Bearer ${parsedToken}`,
+          },
+        })
+        .then((data) => {
+          setPlaylists(data);
+          console.log(data);
+          setHasPlaylists(true);
+        });
+    }
+  };
 
   return (
     <div className="App">
@@ -31,18 +55,36 @@ function App() {
       {isLoggedIn ? (
         <div className="loggedIn">
           <h2>You're logged in, Hi {profile.display_name}!</h2>
-          <img src={profile.images[0].url} alt="" />
+          {profile.images[0].url && <img src={profile.images[0].url} alt="" />}
+          <br />
+          <button
+            style={{ display: "inline-block" }}
+            onClick={() => getPlaylistHandler()}
+          >
+            Get Playlists
+          </button>
         </div>
       ) : (
         <div className="login">
           <button
-            onClick={() =>
-              (window.location = "https://joint-backend.herokuapp.com/login")
-            }
+            onClick={() => (window.location = "http://localhost:8888/login")}
           >
             CLICK HERE TO LOGIN
           </button>
         </div>
+      )}
+      {hasPlaylists ? (
+        <div className="playlists">
+          <h1>we have playlistssss</h1>
+          {playlists.data.items.map((aPlaylist) => (
+            <div className="aplaylist">
+              <input type="checkbox" />
+              {aPlaylist.name}
+            </div>
+          ))}
+        </div>
+      ) : (
+        ""
       )}
     </div>
   );
