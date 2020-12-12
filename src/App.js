b840530ebf5cf99c1b9d7f19c91b3a4a7dac4f85
Ed Tervit <edtervit@hotmail.co.uk>
2020-12-12 20:11:13 +0000

    Working basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import queryString from "query-string";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  //state
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profile, setProfile] = useState({});
+  //useeffect
+  useEffect(() => {
+    let token = queryString.parse(window.location.search);
+    let parsedToken = token.access_token;
+    if (parsedToken) {
+      axios
+        .get("https://api.spotify.com/v1/me", {
+          headers: {
+            Authorization: `Bearer ${parsedToken}`,
+          },
+        })
+        .then((data) => {
+          setProfile(data.data);
+          setIsLoggedIn(true);
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      {isLoggedIn ? (
+        <div className="loggedIn">
+          <h2>You're logged in, Hi {profile.display_name}!</h2>
+          <img src={profile.images[0].url} alt="" />
+        </div>
+      ) : (
+        <div className="login">
+          <button
+            onClick={() => (window.location = "http://localhost:8888/login")}
+          >
+            CLICK HERE TO LOGIN
+          </button>
+        </div>
+      )}
     </div>
   );
 }
