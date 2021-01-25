f8d4fe0190879b6f04ce1ab4aa1e9f3d483a0c25
Ed Tervit <edtervit@hotmail.co.uk>
2021-01-25 17:26:27 +0000

    can select 200, 500 or all liked songs

--- src/components/Create/ProfileBuilder/Sources/LikedSongs.js
+++ src/components/Create/ProfileBuilder/Sources/LikedSongs.js
@@ -26,19 +26,42 @@ function LikedSongs() {
   const token = useStoreState((state) => state.token);
 
   //handlers
+
   const getTheDataHandler = async () => {
-    if (radioValue === "nah") {
-      setGotLikedSongs(true);
-    } else {
-      if (radioValue === "all") {
-        const params = {
-          url: `https://api.spotify.com/v1/me/tracks?offset=0&limit=50`,
+    const getTheTing = async (amount) => {
+      let songLimit = amount - 50 + 1;
+      amount === "all" && (songLimit = 10000);
+      console.log(songLimit);
+      const params = {
+        url: `https://api.spotify.com/v1/me/tracks?offset=0&limit=50`,
+        token: token,
+      };
+      let spotifyResponse = await callAPI(params);
+      console.log(spotifyResponse);
+      let offset = 50;
+
+      if (spotifyResponse) {
+        spotifyResponse.items.forEach((song) => {
+          const songName = song.track.name;
+          const songArtist = song.track.artists[0].name;
+          const songImage = song.track.album.images[0].url;
+          const songUri = song.track.uri;
+          const payload = {
+            name: songName,
+            artist: songArtist,
+            image: songImage,
+            uri: songUri,
+          };
+          //for each song use addtolist action to add it to overall list
+          addToList(payload);
+        });
+      }
+      while (spotifyResponse.next && offset < songLimit) {
+        let newParams = {
+          url: `https://api.spotify.com/v1/me/tracks?offset=${offset}&limit=50`,
           token: token,
         };
-        let spotifyResponse = await callAPI(params);
-        console.log(spotifyResponse);
-        let offset = 50;
-
+        spotifyResponse = await callAPI(newParams);
         if (spotifyResponse) {
           spotifyResponse.items.forEach((song) => {
             const songName = song.track.name;
@@ -55,32 +78,24 @@ function LikedSongs() {
             addToList(payload);
           });
         }
-        while (spotifyResponse.next) {
-          let newParams = {
-            url: `https://api.spotify.com/v1/me/tracks?offset=${offset}&limit=50`,
-            token: token,
-          };
-          spotifyResponse = await callAPI(newParams);
-          if (spotifyResponse) {
-            spotifyResponse.items.forEach((song) => {
-              const songName = song.track.name;
-              const songArtist = song.track.artists[0].name;
-              const songImage = song.track.album.images[0].url;
-              const songUri = song.track.uri;
-              const payload = {
-                name: songName,
-                artist: songArtist,
-                image: songImage,
-                uri: songUri,
-              };
-              //for each song use addtolist action to add it to overall list
-              addToList(payload);
-            });
-          }
-          console.log(spotifyResponse);
-          offset += 50;
-        }
+        console.log(spotifyResponse);
+        offset += 50;
       }
+    };
+
+    if (radioValue === "nah") {
+      setGotLikedSongs(true);
+    } else {
+      if (radioValue === "all") {
+        await getTheTing("all");
+      }
+      if (radioValue === "200") {
+        await getTheTing(200);
+      }
+      if (radioValue === "500") {
+        await getTheTing(500);
+      }
+
       // once job is complete set this to true
       setGotLikedSongs(true);
     }
