9439c9b6812ecdc92c0e3736bc979126acb0748d
Ed Tervit <edtervit@hotmail.co.uk>
2021-02-09 17:20:55 +0000

    added fail cookie back if fail refresh token

--- src/model.js
+++ src/model.js
@@ -188,6 +188,7 @@ const model = {
     if (!res.ok) {
       const data = res.json();
       actions.setIsWaiting(false);
+      actions.failCookie();
       return data;
     } else {
       const data = await res.json();
